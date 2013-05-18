fbeb08626f79780cda00abc857759f09b19405e3
zzmxleo <zzmxleo@gmail.com>
2013-05-18 21:18:25 +0800

    作业提交

---
+++ homework/section_02/div.js
@@ -0,0 +1,97 @@
+function onMouseOver1(){
+	var div1 = document.getElementById("divMenu1");
+	var div2 = document.getElementById("div12");
+	div1.style.display = "block";
+	div2.style.display = "block";
+}
+
+function onMouseOut1(){
+	var div1 = document.getElementById("divMenu1");
+	var div2 = document.getElementById("div12");
+	div1.style.display = "none";
+	div2.style.display = "none";
+}
+
+function onMouseOver2(){
+	var div1 = document.getElementById("divMenu2");
+	var div2 = document.getElementById("div22");
+	div1.style.display = "block";
+	div2.style.display = "block";
+}
+
+function onMouseOut2(){
+	var div1 = document.getElementById("divMenu2");
+	var div2 = document.getElementById("div22");
+	div1.style.display = "none";
+	div2.style.display = "none";
+}
+
+function onMouseOver3(){
+	var div1 = document.getElementById("divMenu3");
+	var div2 = document.getElementById("div32");
+	div1.style.display = "block";
+	div2.style.display = "block";
+}
+
+function onMouseOut3(){
+	var div1 = document.getElementById("divMenu3");
+	var div2 = document.getElementById("div32");
+	div1.style.display = "none";
+	div2.style.display = "none";
+}
+
+function onMouseOver4(){
+	var div1 = document.getElementById("divMenu4");
+	var div2 = document.getElementById("div42");
+	div1.style.display = "block";
+	div2.style.display = "block";
+}
+
+function onMouseOut4(){
+	var div1 = document.getElementById("divMenu4");
+	var div2 = document.getElementById("div42");
+	div1.style.display = "none";
+	div2.style.display = "none";
+}
+
+function onMouseOver5(){
+	var div1 = document.getElementById("divMenu5");
+	var div2 = document.getElementById("div52");
+	div1.style.display = "block";
+	div2.style.display = "block";
+}
+
+function onMouseOut5(){
+	var div1 = document.getElementById("divMenu5");
+	var div2 = document.getElementById("div52");
+	div1.style.display = "none";
+	div2.style.display = "none";
+}
+
+function onMouseOver6(){
+	var div1 = document.getElementById("divMenu6");
+	var div2 = document.getElementById("div62");
+	div1.style.display = "block";
+	div2.style.display = "block";
+}
+
+function onMouseOut6(){
+	var div1 = document.getElementById("divMenu6");
+	var div2 = document.getElementById("div62");
+	div1.style.display = "none";
+	div2.style.display = "none";
+}
+
+function onMouseOver7(){
+	var div1 = document.getElementById("divMenu7");
+	var div2 = document.getElementById("div72");
+	div1.style.display = "block";
+	div2.style.display = "block";
+}
+
+function onMouseOut7(){
+	var div1 = document.getElementById("divMenu7");
+	var div2 = document.getElementById("div72");
+	div1.style.display = "none";
+	div2.style.display = "none";
+}
